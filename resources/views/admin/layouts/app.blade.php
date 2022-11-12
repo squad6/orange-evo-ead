aa9efe09bc1558a37d6730461b3ace515057e9dd
Ariel Vieira <arielvieira65@gmail.com>
2022-11-12 07:45:01 -0300

    :boom: fix: Removendo link do css do bootstrap

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -20,7 +20,7 @@
         <link href="{{ url('assets/css/sb-admin-2.min.css') }}" rel="stylesheet">
         <link href="{{ url('assets/css/login-admin.css') }}" rel="stylesheet">
 
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
+        {{-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css"> --}}
 
     </head>
 
